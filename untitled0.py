35e5a7a5e396da147a1b9b4df30a30e22f46d856
Jayantika2010 <79147549+jayantika51@users.noreply.github.com>
2023-06-08 11:47:07 +0530

    Add files via upload

---
+++ untitled0.py
@@ -0,0 +1,27 @@
+class History_in_space():
+    def year_2008(self):
+        print("\n"," Achievements occurred in year 2008","\n")
+        print("India successfully launched Chandrayaan in 2008 in order to explore the moon.The craft was inserted into lunar orbit in its first attempt. One of the greatest achievement of Chandrayaan was the discovery of the water molecules in the lunar soil.")
+        
+
+    def year_2011(self):
+        print("\n","Eventes ocured in the field of space in 2011","\n")
+    
+        
+
+class History_in_medical():
+    def year_2008(self):
+        print("\n","Achievements occurred in year 2008","\n")
+        print("Although scientists have long discovered how malaria is transferred and know how to prevent it, nearly 1 million people die every year from the disease, according to international estimates.Finally, Dec. 8, the first results of a malaria vaccine that shows promise hit international news.")
+        
+        
+    def year_2011(self):
+        print("\n","Event occured in the field of medical","\n")
+        
+class History_in_sports():
+    def year_2008(self):
+        print("\n","Achievements occurred in year 2008","\n")
+        print("In 2008, Abhinav Bindra created history by bagging gold in men's 10m air rifle event at the Beijing Games. His gold remains the country's first and only individual gold medal till date.")
+        
+    def year_2011(self): 
+        print("\n","Event occured in the field of sports","\n")
